728045762df2588dc7cfb69c9fb33e9638a04d19
gorb6593 <gorb6593@gmail.com>
2023-07-21 01:10:00 +0900

    setting

---
+++ haekyu006/src/main/java/haekyu006/Haekyu006Application.java
@@ -0,0 +1,13 @@
+package haekyu006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Haekyu006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Haekyu006Application.class, args);
+	}
+
+}
---
+++ haekyu006/src/test/java/haekyu006/Haekyu006ApplicationTests.java
@@ -0,0 +1,13 @@
+package haekyu006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Haekyu006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
