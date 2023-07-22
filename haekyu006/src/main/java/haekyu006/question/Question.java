020bd2b1ff19c14e605c58fc63ca9b04ee226cc8
gorb6593 <gorb6593@gmail.com>
2023-07-23 02:16:34 +0900

    테스트코드추가

---
+++ haekyu006/src/main/java/haekyu006/question/Question.java
@@ -0,0 +1,28 @@
+package haekyu006.question;
+
+import haekyu006.answer.Answer;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Data
+@Entity
+// 아래 Question 클래스는 엔티티 클래스이다.
+// 아래 클래스와 1:1로 매칭되는 테이블이 DB 없다면, 자동으로 생성되어야 한다.
+public class Question {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private Integer id;
+
+    @Column(length = 200) // varchar(200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
