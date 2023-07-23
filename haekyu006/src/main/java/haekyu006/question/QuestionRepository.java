bb3548b1999a476d3543e954086c9b74c28de511
gorb6593 <gorb6593@gmail.com>
2023-07-23 18:02:22 +0900

    db연동

--- haekyu006/src/main/java/haekyu006/question/QuestionRepository.java
+++ haekyu006/src/main/java/haekyu006/question/QuestionRepository.java
@@ -1,7 +1,19 @@
 package haekyu006.question;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
+    Question findBySubject(String subject);
+
+    @Modifying
+    @Transactional
+    @Query(value = "TRUNCATE question", nativeQuery = true)
+    void truncate();
 }
--- haekyu006/src/test/java/haekyu006/Haekyu006ApplicationTests.java
+++ haekyu006/src/test/java/haekyu006/Haekyu006ApplicationTests.java
@@ -7,30 +7,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Haekyu006ApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+
 	@Test
-	void testJpa() {
+	void testJpa0() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+		questionRepository.save(q1);  // 첫번째 질문 저장
+
+		System.out.println(q1.getId());
 
 		Question q2 = new Question();
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		questionRepository.save(q2);  // 두번째 질문 저장
+
+		System.out.println(q2.getId());
+
+		questionRepository.truncate();
+	}
+	@Test
+	void testJpa1() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		System.out.println(q1.getId());
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
+
+		System.out.println(q2.getId());
 
 		assertThat(q1.getId()).isGreaterThan(0);
 		assertThat(q2.getId()).isGreaterThan(q1.getId());
+	}
+
+	@Test
+	void testJpa2() {
+
+		//select * from question
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void testJpa3() {
+		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
 	}
 
 }
